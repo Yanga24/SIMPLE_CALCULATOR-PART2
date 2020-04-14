13661c9bc5627e7aad7252195c91e85e88b6fc92
Yanga24 <60698180+Yanga24@users.noreply.github.com>
2020-04-14 02:35:01 +0200

    first commit

---
+++ src/simple_Calculator part2.js
@@ -0,0 +1,57 @@
+class Calculator {
+  constructor() {
+    this.lastNum = 0;
+    this.slot = [];
+  }
+
+  add(...args) {
+    let sum = 0;
+
+    for (let num of args) {
+      if (num == 'Last') {
+        num = this.last();
+      } else if (num == 'slot_' + num.toString().charAt(num.length - 1)) {
+        sum += this.get_slot(num.charAt(num.length - 1));
+      } else if (!isNaN(num)) {
+        sum += num;
+      }
+    }
+
+    this.slot.push(sum);
+    return sum;
+  }
+
+  multiply(...args) {
+    let product = 1;
+
+    for (let num of args) {
+      if (num == 'Last') {
+        num = this.last();
+      } else if (num == 'slot_' + num.toString().charAt(num.length - 1)) {
+        product *= this.get_slot(num.charAt(num.length - 1));
+      } else if (!isNaN(num)) {
+        product *= num;
+      }
+    }
+    
+    this.slot.push(product);
+    return product;
+  }
+
+  last() {
+    return this.lastNum;
+  }
+
+  set_slot(num) {
+    this.lastNum = this.slot[num - 1];
+  }
+
+  get_slot(num) {
+    return this.slot[num - 1];
+  }
+}
+
+module.exports = Calculator;
+
+  
+
